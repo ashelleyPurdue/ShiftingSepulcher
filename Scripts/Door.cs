ff77c8a195b0448842cae72e3c5bb50ef965a2e2
Alex Shelley <alexthepeppershelley@gmail.com>
2022-08-18 23:30:00 -0400

    Locked doors now display which key they use

--- Scripts/Door.cs
+++ Scripts/Door.cs
@@ -9,13 +9,20 @@ namespace RandomDungeons
 
         public override void _Process(float delta)
         {
+            // Hide or show the door
             var transform = Transform;
 
-            transform.Scale = GraphDoor.Destination != null
+            transform.Scale = GraphDoor.Destination != null && !GraphDoor.IsLocked
                 ? Vector2.Zero
                 : Vector2.One;
 
             Transform = transform;
+
+            // Display which lock is on this door
+            var label = GetNode<Label>("Label");
+            GetNode<Label>("Label").Text = GraphDoor.IsLocked
+                ? $"Lock {GraphDoor.LockId}"
+                : "";
         }
     }
 }

4c292dbb229df93691509f54057bbc3e4d67ee77
Alex Shelley <alexthepeppershelley@gmail.com>
2022-08-27 17:01:18 -0400

    Add more constants to Vector2i

--- Scripts/MathUtils/Vector2i.cs
+++ Scripts/MathUtils/Vector2i.cs
@@ -8,6 +8,12 @@ namespace RandomDungeons.MathUtils
         public int y;
 
         public static Vector2i Zero => new Vector2i(0, 0);
+        public static Vector2i One => new Vector2i(1, 1);
+
+        public static Vector2i Up => new Vector2i(0, 1);
+        public static Vector2i Down => new Vector2i(0, -1);
+        public static Vector2i Left => new Vector2i(-1, 0);
+        public static Vector2i Right => new Vector2i(1, 0);
 
         public Vector2i(int x, int y)
         {

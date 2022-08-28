75c9d71f0a3895b296f5c3f0dee4ca7ec4d83ccd
Alex Shelley <ashelley@purdue.edu>
2022-08-28 18:54:06 -0400

    Add a Generate() method, but it doesn't guarantee solvability yet.

--- Scripts/Graphs/SlidingIceGraph.cs
+++ Scripts/Graphs/SlidingIceGraph.cs
@@ -13,8 +13,46 @@ namespace RandomDungeons.Graphs
         public readonly Vector2i StartPos;
         public Vector2i EndPos {get; private set;}
 
-        public IEnumerable<Vector2i> BlockPositions => _blockPositions;
-        private HashSet<Vector2i> _blockPositions = new HashSet<Vector2i>();
+        public IEnumerable<Vector2i> RockPositions => _rockPositions;
+        private HashSet<Vector2i> _rockPositions = new HashSet<Vector2i>();
+
+        public static SlidingIceGraph Generate(
+            int seed,
+            int width,
+            int height,
+            int numPushes
+        )
+        {
+            // TODO: use The Algorithm (tm) to generate an always-completable
+            // puzzle.
+            // Right now, we're just randomly throwing things in there without
+            // a care.
+
+            var rng = new Random(seed);
+
+            var startPos = new Vector2i(
+                rng.Next(0, width),
+                rng.Next(0, height)
+            );
+            var endPos = new Vector2i(
+                rng.Next(0, width),
+                rng.Next(0, height)
+            );
+
+            var graph = new SlidingIceGraph(width, height, startPos);
+            graph.EndPos = endPos;
+
+            for (int i = 0; i < numPushes; i++)
+            {
+                var rockPos = new Vector2i(
+                    rng.Next(0, width),
+                    rng.Next(0, height)
+                );
+                graph._rockPositions.Add(rockPos);
+            }
+
+            return graph;
+        }
 
         public SlidingIceGraph(int width, int height, Vector2i startPos)
         {

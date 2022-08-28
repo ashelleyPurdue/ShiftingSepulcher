312410f56f7748acdf048611479712854baeec36
Alex Shelley <ashelley@purdue.edu>
2022-08-28 19:25:07 -0400

    Set up the test map node to generate a puzzle.

--- Scripts/Nodes/Maps/SlidingIcePuzzleTestMap.cs
+++ Scripts/Nodes/Maps/SlidingIcePuzzleTestMap.cs
@@ -1,9 +1,22 @@
 using Godot;
 
+using RandomDungeons.Graphs;
+using RandomDungeons.PhysicalDungeons;
+
 namespace RandomDungeons.Nodes.Maps
 {
     public class SlidingIcePuzzleTestMap : Node2D
     {
-        
+        public override void _Ready()
+        {
+            var graph = SlidingIceGraph.Generate(
+                seed: 1337,
+                width: 10,
+                height: 10,
+                numPushes: 3
+            );
+
+            GetNode<SlidingIcePuzzle>("%SlidingIcePuzzle").SetGraph(graph);
+        }
     }
 }

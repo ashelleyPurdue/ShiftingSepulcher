f7f4b2cd219e1e5be1066029dab92176f28b44e6
Alex Shelley <alexthepeppershelley@gmail.com>
2022-08-28 21:22:25 -0400

    Add some functions to aid in generating the puzzle

--- Scripts/Graphs/SlidingIceGraph.cs
+++ Scripts/Graphs/SlidingIceGraph.cs
@@ -62,5 +62,52 @@ namespace RandomDungeons.Graphs
             StartPos = startPos;
             EndPos = startPos;
         }
+
+        private void Push(Vector2i dir, int dist)
+        {
+            // Slide the goal in some direction, as if we were pushing the ice
+            // block.
+            EndPos += dir * dist;
+
+            // Place a rock in the tile right after it, to stop the imaginary
+            // ice block
+            _rockPositions.Add(EndPos + dir);
+        }
+
+        /// <summary>
+        /// Returns the maximum number of tiles an imaginary ice block, starting
+        /// at <paramref name="pos"/>, can be pushed in direction
+        /// <paramref name="dir"/>, before it hits a rock or the edge of the
+        /// board.
+        /// </summary>
+        /// <param name="pos"></param>
+        /// <param name="dir"></param>
+        /// <returns></returns>
+        private int MaxPushDistance(Vector2i pos, Vector2i dir)
+        {
+            int dist = 0;
+
+            Vector2i nextPos = pos + dir;
+            while (IsInBounds(nextPos) && !IsRock(nextPos))
+            {
+                dist++;
+                pos += dir;
+                nextPos += dir;
+            }
+
+            return dist;
+        }
+
+        private bool IsInBounds(Vector2i pos)
+        {
+            return
+                (pos.x >= 0 && pos.y >= 0) &&
+                (pos.x < Width && pos.y < Height);
+        }
+
+        private bool IsRock(Vector2i pos)
+        {
+            return _rockPositions.Contains(pos);
+        }
     }
 }

d9933744af8471e3fdaeaa904e09470fef541f83
Alex Shelley <alexthepeppershelley@gmail.com>
2022-08-30 17:11:04 -0400

    Add a reset button

--- Scripts/Nodes/PhysicalDungeons/SlidingIcePuzzle.cs
+++ Scripts/Nodes/PhysicalDungeons/SlidingIcePuzzle.cs
@@ -20,29 +20,43 @@ namespace RandomDungeons.PhysicalDungeons
         private Node2D _eastBorder => GetNode<Node2D>("%EastBorder");
         private Node2D _westBorder => GetNode<Node2D>("%WestBorder");
 
+        private SlidingIceGraph _graph;
+
         public void SetGraph(SlidingIceGraph graph)
         {
-            // Resize the floor to match the graph's size
-            _floor.MarginRight = graph.Width * 32;
-            _floor.MarginBottom = graph.Width * 32;
+            _graph = graph;
+            ResetPuzzle();
+        }
 
-            // TODO: Clear out existing children
+        public void ResetPuzzle()
+        {
+            // Remove all existing elements
+            while (_puzzleElements.GetChildCount() > 0)
+            {
+                var child = _puzzleElements.GetChild(0);
+                _puzzleElements.RemoveChild(child);
+                child.QueueFree();
+            }
+
+            // Resize the floor to match the graph's size
+            _floor.MarginRight = _graph.Width * 32;
+            _floor.MarginBottom = _graph.Width * 32;
 
             // Add the surrounding dirt-patches
-            _northBorder.Scale = new Vector2(graph.Width, 1);
-            _southBorder.Scale = new Vector2(graph.Width, 1);
-            _eastBorder.Scale  = new Vector2(1, graph.Height);
-            _westBorder.Scale  = new Vector2(1, graph.Height);
+            _northBorder.Scale = new Vector2(_graph.Width, 1);
+            _southBorder.Scale = new Vector2(_graph.Width, 1);
+            _eastBorder.Scale  = new Vector2(1, _graph.Height);
+            _westBorder.Scale  = new Vector2(1, _graph.Height);
 
-            _southBorder.Position = new Vector2(0, graph.Height) * 32;
-            _eastBorder.Position  = new Vector2(graph.Width, 0) * 32;
+            _southBorder.Position = new Vector2(0, _graph.Height) * 32;
+            _eastBorder.Position  = new Vector2(_graph.Width, 0) * 32;
 
             // Place the ice block and goal
-            Create(IceBlockPrefab, graph.StartPos);
-            Create(EndingSlotPrefab, graph.EndPos);
+            Create(IceBlockPrefab, _graph.StartPos);
+            Create(EndingSlotPrefab, _graph.EndPos);
 
             // Place all the rocks
-            foreach (var rockPos in graph.RockPositions)
+            foreach (var rockPos in _graph.RockPositions)
             {
                 Create(RockPrefab, rockPos);
             }

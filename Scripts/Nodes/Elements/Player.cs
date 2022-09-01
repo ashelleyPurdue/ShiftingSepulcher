e98481e2d55a88b0835b5ec01827fcced1efd3ee
Alex Shelley <alexthepeppershelley@gmail.com>
2022-09-01 19:52:16 -0400

    You can't go twice as fast by moving diagonal anymore.  Sorry.

--- Scripts/Nodes/Elements/Player.cs
+++ Scripts/Nodes/Elements/Player.cs
@@ -10,7 +10,18 @@ namespace RandomDungeons.Nodes.Elements
 
         public override void _Process(float deltaTime)
         {
-            this.MoveAndSlide(InputService.LeftStick * WalkSpeed);
+            var cappedLeftStick = CapMagnitude(InputService.LeftStick, 1);
+            this.MoveAndSlide(cappedLeftStick * WalkSpeed);
+        }
+
+        private Vector2 CapMagnitude(Vector2 v, float maxMagnitude)
+        {
+            float magnitude = v.Length();
+
+            if (magnitude > maxMagnitude)
+                magnitude = maxMagnitude;
+
+            return v.Normalized() * magnitude;
         }
     }
 }

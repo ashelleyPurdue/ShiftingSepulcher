1cbe660f37e21c7fd4e2db1c561d8ce3ad59bc83
Alex Shelley <alexthepeppershelley@gmail.com>
2022-09-03 17:25:28 -0400

    Add EightDirection.ToVector2

--- Scripts/Utils/EightDirection.cs
+++ Scripts/Utils/EightDirection.cs
@@ -1,3 +1,5 @@
+using Godot;
+
 namespace RandomDungeons.Utils
 {
     public enum EightDirection
@@ -10,5 +12,25 @@ namespace RandomDungeons.Utils
         SouthWest = 5,
         West = 6,
         NorthWest = 7
+    }
+
+    public static class EightDirectionExtensions
+    {
+        public static Vector2 ToVector2(this EightDirection dir)
+        {
+            switch (dir)
+            {
+                case EightDirection.North: return Vector2.Up;
+                case EightDirection.NorthEast: return (Vector2.Up + Vector2.Right).Normalized();
+                case EightDirection.East: return Vector2.Right;
+                case EightDirection.SouthEast: return (Vector2.Down + Vector2.Right).Normalized();
+                case EightDirection.South: return Vector2.Down;
+                case EightDirection.SouthWest: return (Vector2.Down + Vector2.Left).Normalized();
+                case EightDirection.West: return Vector2.Left;
+                case EightDirection.NorthWest: return (Vector2.Up + Vector2.Left).Normalized();
+
+                default: return Vector2.Zero;
+            }
+        }
     }
 }

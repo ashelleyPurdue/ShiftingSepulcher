b96426aeb3d6b9fca6783625bb077823914e82b4
Alex Shelley <alexthepeppershelley@gmail.com>
2022-09-05 17:46:59 -0400

    Rewrite the player to be based on a state machine

--- Scripts/Nodes/Elements/Player.cs
+++ Scripts/Nodes/Elements/Player.cs
@@ -4,44 +4,93 @@ using RandomDungeons.Services;
 using RandomDungeons.Nodes.Components;
 using RandomDungeons.Utils;
 
+using RandomDungeons.StateMachines;
+using RandomDungeons.StateMachines.CommonStates;
+
 namespace RandomDungeons.Nodes.Elements
 {
-    public class Player : KinematicBody2D
+    public class Player : KinematicBody2D, IStateMachine
     {
         public const float WalkSpeed = 283;
 
         private EightDirectionalSprite _sprite => GetNode<EightDirectionalSprite>("%Sprite");
         private PlayerSword _sword => GetNode<PlayerSword>("%Sword");
 
+        private IState _currentState;
+
+        public override void _Ready()
+        {
+            DeathAnimation.AnimationTarget = _sprite;
+            DeathAnimation.AnimationEnded += () =>
+            {
+                GetTree().ChangeScene("res://Maps/TitleScreen.tscn");
+            };
+
+            ChangeState(Walking);
+        }
+
+        public void ChangeState(IState state)
+        {
+            if (state != null)
+                state.Owner = this;
+
+            var prevState = _currentState;
+            _currentState = state;
+
+            prevState?._StateExited();
+            _currentState?._StateEntered();
+        }
+
         public override void _Process(float deltaTime)
         {
-            SwordControls(deltaTime);
-            WalkControls(deltaTime);
+            _currentState?._Process(deltaTime);
         }
 
-        private void SwordControls(float deltaTime)
+        public override void _PhysicsProcess(float deltaTime)
         {
-            if (InputService.AttackPressed && !_sword.IsSwinging)
-                _sword.StartSwinging(_sprite.Direction);
+            _currentState._PhysicsProcess(deltaTime);
         }
 
-        private void WalkControls(float deltaTime)
+        private readonly IState Walking = new WalkingState();
+        private class WalkingState : State<Player>
         {
-            if (_sword.IsSwinging)
+            public override void _Process(float delta)
             {
-                _sprite.SpeedScale = 0;
-                return;
+                if (InputService.AttackPressed && !Owner._sword.IsSwinging)
+                    ChangeState(Owner.SwingingSword);
             }
 
-            // Move with the left stick
-            var cappedLeftStick = InputService.LeftStick.LimitLength(1);
-            this.MoveAndSlide(cappedLeftStick * WalkSpeed);
+            public override void _PhysicsProcess(float delta)
+            {
+                // Move with the left stick
+                var cappedLeftStick = InputService.LeftStick.LimitLength(1);
+                Owner.MoveAndSlide(cappedLeftStick * WalkSpeed);
 
-            // Update the sprite
-            if (cappedLeftStick.Length() > 0.01)
-                _sprite.Direction = cappedLeftStick.ToNearestEightDirection();
+                // Update the sprite
+                if (cappedLeftStick.Length() > 0.01)
+                    Owner._sprite.Direction = cappedLeftStick.ToNearestEightDirection();
 
-            _sprite.SpeedScale = cappedLeftStick.Length();
+                Owner._sprite.SpeedScale = cappedLeftStick.Length();
+            }
         }
+
+        private readonly IState SwingingSword = new SwingingSwordState();
+        private class SwingingSwordState : State<Player>
+        {
+            public override void _StateEntered()
+            {
+                Owner._sword.StartSwinging(Owner._sprite.Direction);
+                Owner._sprite.SpeedScale = 0;
+            }
+
+            public override void _PhysicsProcess(float delta)
+            {
+                if (!Owner._sword.IsSwinging)
+                    ChangeState(Owner.Walking);
+            }
+        }
+
+        private readonly DeathAnimationState DeathAnimation = new DeathAnimationState();
+
     }
 }

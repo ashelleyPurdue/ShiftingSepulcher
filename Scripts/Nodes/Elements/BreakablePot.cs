16ecda7a520e576c5ab80b6eef1319ca2870de6a
Alex Shelley <alexthepeppershelley@gmail.com>
2022-09-04 21:21:59 -0400

    Pots now get knocked back by the sword, and only break when hitting something solid

--- Scripts/Nodes/Elements/BreakablePot.cs
+++ Scripts/Nodes/Elements/BreakablePot.cs
@@ -1,13 +1,39 @@
 using Godot;
 
 using RandomDungeons.Nodes.Components;
+
 namespace RandomDungeons.Nodes.Elements
 {
-    public class BreakablePot : Node2D
+    public class BreakablePot : KinematicBody2D
     {
+        private const float Friction = 500;
+        private const float KnockbackSpeed = 300;
+        private const float MinSpeedForCollisionDamage = 90;
+
+        private Vector2 _velocity = Vector2.Zero;
+
+        public override void _PhysicsProcess(float delta)
+        {
+            var prevVel = _velocity;
+            _velocity = MoveAndSlide(_velocity);
+
+            // Take damage when hitting a wall
+            bool wasGoingFastEnough = prevVel.Length() > MinSpeedForCollisionDamage;
+
+            if (GetSlideCount() > 0 && wasGoingFastEnough)
+            {
+                QueueFree();
+                return;
+            }
+
+            // Apply friction
+            _velocity = _velocity.MoveToward(Vector2.Zero, Friction * delta);
+        }
+
         public void OnTookDamage(HitBox hitBox)
         {
-            QueueFree();
+            var dir = hitBox.GlobalPosition.DirectionTo(GlobalPosition);
+            _velocity = KnockbackSpeed * dir;
         }
     }
 }

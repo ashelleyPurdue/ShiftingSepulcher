0d80b7b2964741018e34741532373d7b20e4adc3
Alex Shelley <alexthepeppershelley@gmail.com>
2022-09-04 21:41:32 -0400

    Pots now have health.  HitBoxes now have damage.
    The HitBox also determines how many i-frames the target receives when hit.

--- Scripts/Nodes/Components/HitBox.cs
+++ Scripts/Nodes/Components/HitBox.cs
@@ -5,7 +5,9 @@ namespace RandomDungeons.Nodes.Components
     public class HitBox : Area2D
     {
         [Signal] public delegate void DealtDamage(HurtBox hurtBox);
-        [Export] public Vector2 KnockbackVelocity;
+        [Export] public int Damage = 1;
+        [Export] public float InvlunerabilityTime = 1;
+        [Export] public Vector2 KnockbackVelocity = Vector2.Zero;
 
         public override void _PhysicsProcess(float delta)
         {
--- Scripts/Nodes/Elements/BreakablePot.cs
+++ Scripts/Nodes/Elements/BreakablePot.cs
@@ -9,29 +9,45 @@ namespace RandomDungeons.Nodes.Elements
         private const float Friction = 500;
         private const float MinSpeedForCollisionDamage = 90;
 
+        [Export] public int Health = 2;
+
         private Vector2 _velocity = Vector2.Zero;
+        private float _invulnerableTimer = 0;
 
         public override void _PhysicsProcess(float delta)
         {
+            _invulnerableTimer -= delta;
+
+            // Move
             var prevVel = _velocity;
             _velocity = MoveAndSlide(_velocity);
+            _velocity = _velocity.MoveToward(Vector2.Zero, Friction * delta);
 
             // Take damage when hitting a wall
             bool wasGoingFastEnough = prevVel.Length() > MinSpeedForCollisionDamage;
 
             if (GetSlideCount() > 0 && wasGoingFastEnough)
             {
-                QueueFree();
+                Health--;
                 return;
             }
 
-            // Apply friction
-            _velocity = _velocity.MoveToward(Vector2.Zero, Friction * delta);
+            // Die when out of health
+            if (Health <= 0)
+                QueueFree();
         }
 
         public void OnTookDamage(HitBox hitBox)
         {
+            // Invulnerability only protects you from damage.
+            // It doesn't protect you from knockback.
             _velocity = hitBox.KnockbackVelocity;
+
+            if (_invulnerableTimer > 0)
+                return;
+
+            Health -= hitBox.Damage;
+            _invulnerableTimer = hitBox.InvlunerabilityTime;
         }
     }
 }

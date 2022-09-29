48aec9e6b4dbbe4a2270830eb17784b32d9b213c
Alex Shelley <alexthepeppershelley@gmail.com>
2022-09-29 17:39:55 -0400

    SummonTile() now actually summons a tile

--- Scripts/Nodes/Bosses/Tilemancer.cs
+++ Scripts/Nodes/Bosses/Tilemancer.cs
@@ -1,14 +1,28 @@
 using Godot;
 
+using RandomDungeons.Utils;
 using RandomDungeons.Nodes.Elements.Enemies;
 
 namespace RandomDungeons.Nodes.Bosses
 {
     public class Tilemancer : AnimationPlayer
     {
+        [Export] public PackedScene TilePrefab;
+        [Export] public float ArenaHeight = 32 * 16;
+        [Export] public float ArenaWidth = 32 * 16;
+
         public void SummonTile()
         {
-            GD.Print("Fwip!");
+            Vector2 tilePos = new Vector2(
+                (GD.Randf() * ArenaWidth) - (ArenaWidth / 2),
+                (GD.Randf() * ArenaHeight) - (ArenaHeight / 2)
+            );
+
+            var tile = TilePrefab.Instance<AnimatedTile>();
+            GetParent().AddChild(tile);
+            tile.Position = tilePos;
+            tile.Target = GetTree().FindPlayer();
+            tile.HoldTime = 1.6f;
         }
 
         public void ThrowTile()

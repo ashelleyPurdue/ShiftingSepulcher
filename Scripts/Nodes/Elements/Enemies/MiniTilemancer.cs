95e5c5bcb30e93e1edd6633d6558eead5f0532c7
Alex Shelley <alexthepeppershelley@gmail.com>
2022-10-01 18:08:25 -0400

    Delete most things about the mini tilemancer

--- Scripts/Nodes/Elements/Enemies/MiniTilemancer.cs
+++ Scripts/Nodes/Elements/Enemies/MiniTilemancer.cs
@@ -5,108 +5,15 @@ using RandomDungeons.Utils;
 
 namespace RandomDungeons.Nodes.Elements.Enemies
 {
-    public class MiniTilemancer : BaseEnemy
+    public class MiniTilemancer : KinematicBody2D
     {
         [Export] public float TileSpawnRadius = 32 * 2;
-        [Export] public float TileSpinUpTime = 1;
-        [Export] public float TileHoldTime = 2;
-        [Export] public float TileSpawnCooldownTime = 1;
-        [Export] public float TileFlySpeed = 600;
-
         [Export] public float WanderSpeed = 32;
-        [Export] public float WanderTime = 1;
-        [Export] public float WanderPauseTime = 1;
-
+        [Export] public float TileThrowSpeed = 32 * 19;
         [Export] public PackedScene TilePrefab;
 
         private Node2D _target => GetTree().FindPlayer();
 
-        protected override HurtBox Hurtbox() => GetNode<HurtBox>("%HurtBox");
-        protected override Node2D Visuals() => GetNode<Node2D>("%Visuals");
-        protected override IState InitialState() => SummoningTile;
-
-        private readonly IState SummoningTile = new SummoningTileState();
-        private class SummoningTileState : State<MiniTilemancer>
-        {
-            private float _timer;
-
-            public override void _StateEntered()
-            {
-                _timer =
-                    Owner.TileSpinUpTime +
-                    Owner.TileHoldTime +
-                    Owner.TileSpawnCooldownTime;
-
-                Owner.SummonTile();
-            }
-
-            public override void _PhysicsProcess(float delta)
-            {
-                _timer -= delta;
-
-                if (_timer <= 0)
-                    ChangeState(Owner.Wandering);
-            }
-        }
-
-        private readonly IState Wandering = new WanderingState();
-        private class WanderingState : State<MiniTilemancer>
-        {
-            private float _timer;
-            private Vector2 _velocity;
-
-            public override void _StateEntered()
-            {
-                _timer = Owner.WanderTime;
-
-                float angle = Mathf.Deg2Rad(GD.Randf() * 360);
-                _velocity = Owner.WanderSpeed * new Vector2(
-                    Mathf.Cos(angle),
-                    Mathf.Sin(angle)
-                );
-            }
-
-            public override void _PhysicsProcess(float delta)
-            {
-                Owner.MoveAndSlide(_velocity);
-
-                _timer -= delta;
-
-                if (_timer <= 0)
-                    ChangeState(Owner.Pausing);
-            }
-        }
-
-        private readonly IState Pausing = new PausingState();
-        private class PausingState : State<MiniTilemancer>
-        {
-            private float _timer;
-
-            public override void _StateEntered()
-            {
-                _timer = Owner.WanderPauseTime;
-            }
-
-            public override void _PhysicsProcess(float delta)
-            {
-                _timer -= delta;
-
-                if (_timer <= 0)
-                    ChangeState(Owner.SummoningTile);
-            }
-        }
-
-        private void SummonTile()
-        {
-            var tile = TilePrefab.Instance<AnimatedTile>();
-            GetParent().AddChild(tile);
-
-            tile.GlobalPosition = RandomTileSpawnPos();
-            tile.Target = _target;
-            tile.SpinUpTime = TileSpinUpTime;
-            tile.HoldTime = TileHoldTime;
-            tile.FlySpeed = TileFlySpeed;
-        }
 
         private Vector2 RandomTileSpawnPos()
         {

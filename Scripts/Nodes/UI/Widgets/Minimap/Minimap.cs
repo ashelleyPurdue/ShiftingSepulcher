f4b3c38c8ea8f6d4ac29ca9eac86778aa3905c1f
Alex Shelley <alexthepeppershelley@gmail.com>
2022-10-07 20:54:04 -0400

    It now centers the map

--- Scripts/Nodes/UI/Widgets/Minimap/Minimap.cs
+++ Scripts/Nodes/UI/Widgets/Minimap/Minimap.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using Godot;
 using RandomDungeons.Graphs;
 
@@ -8,23 +10,36 @@ namespace RandomDungeons.Nodes.UI.Widgets.Minimap
         private const float RoomSize = 5 * 32;
 
         [Export] public PackedScene RoomDisplayPrefab;
+        private Node2D _rooms => GetNode<Node2D>("%Rooms");
 
         public void SetGraph(DungeonGraph graph)
         {
-            foreach (var child in GetChildren())
+            // Clear out old rooms
+            foreach (var child in _rooms.GetChildren())
             {
                 ((Node)child).QueueFree();
             }
 
+            // Create new rooms
             foreach (var c in graph.AllRoomCoordinates())
             {
                 var graphRoom = graph.GetRoom(c);
                 var roomDisplay = RoomDisplayPrefab.Instance<RoomDisplay>();
 
-                AddChild(roomDisplay);
+                _rooms.AddChild(roomDisplay);
                 roomDisplay.Position = RoomSize * new Vector2(c.x, -c.y);
                 roomDisplay.SetGraphRoom(graphRoom);
             }
+
+            // Keep the rooms centered
+            var xValues = graph.AllRoomCoordinates().Select(c => c.x * RoomSize);
+            var yValues = graph.AllRoomCoordinates().Select(c => c.y * RoomSize);
+
+            Vector2 center = new Vector2(
+                (xValues.Min() + xValues.Max()) / 2,
+                -(yValues.Min() + yValues.Max()) / 2
+            );
+            _rooms.Position = -center;
         }
     }
 }

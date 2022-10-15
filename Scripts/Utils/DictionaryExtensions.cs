3f7e6356ba51cc24a28fb9467956f8f923c915ed
Alex Shelley <alexthepeppershelley@gmail.com>
2022-10-15 17:40:17 -0400

    Add DictionaryExtensions Invert() and Clone()

---
+++ Scripts/Utils/DictionaryExtensions.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+
+namespace RandomDungeons.Utils
+{
+    public static class DictionaryExtensions
+    {
+        public static Dictionary<TValue, TKey> Invert<TKey, TValue>(
+            this Dictionary<TKey, TValue> dict
+        )
+        {
+            var reversed = new Dictionary<TValue, TKey>();
+            foreach (var pair in dict)
+            {
+                reversed[pair.Value] = pair.Key;
+            }
+
+            return reversed;
+        }
+
+        public static Dictionary<TKey, TValue> Clone<TKey, TValue>(
+            this Dictionary<TKey, TValue> dict
+        )
+        {
+            var clone = new Dictionary<TKey, TValue>();
+
+            foreach (var pair in dict)
+                clone[pair.Key] = pair.Value;
+
+            return clone;
+        }
+    }
+}

5e4ef4bd028f71b422bd45bb8172e422f99bf993
Alex Shelley <ashelley@purdue.edu>
2022-10-16 20:09:24 -0400

    The previous room now lingers, so I can animate it

--- Scripts/Nodes/PhysicalDungeons/RoomTransitionManager.cs
+++ Scripts/Nodes/PhysicalDungeons/RoomTransitionManager.cs
@@ -10,12 +10,15 @@ namespace RandomDungeons.PhysicalDungeons
 {
     public class RoomTransitionManager : Node
     {
+        private Node2D _activeRoomHolder => GetNode<Node2D>("%ActiveRoomHolder");
+        private Node2D _previousRoomHolder => GetNode<Node2D>("%PreviousRoomHolder");
         private Camera2D _camera => GetNode<Camera2D>("%Camera");
 
         private Dictionary<DungeonGraphRoom, IDungeonRoom> _graphRoomToRealRoom;
         private Dictionary<IDungeonRoom, DungeonGraphRoom> _realRoomToGraphRoom;
 
         private IDungeonRoom _activeRoom;
+        private IDungeonRoom _prevRoom;
 
         public void SetGraph(
             DungeonGraph graph,
@@ -55,16 +58,29 @@ namespace RandomDungeons.PhysicalDungeons
 
         private void EnterRoom(DungeonGraphRoom room, Vector2 position)
         {
-            // TODO: Pause the previous room
             // TODO: Start the animation fading the previous room out
             // TODO: Deal with edge cases
 
+            if (_prevRoom != null)
+            {
+                _previousRoomHolder.RemoveChild(_prevRoom.Node);
+                _prevRoom = null;
+            }
+
             if (_activeRoom != null)
-                RemoveChild(_activeRoom.Node);
+            {
+                _activeRoomHolder.RemoveChild(_activeRoom.Node);
+                _previousRoomHolder.AddChild(_activeRoom.Node);
+                _prevRoom = _activeRoom;
+
+                SetNodePaused(_prevRoom.Node, true);
+            }
             
             _activeRoom = _graphRoomToRealRoom[room];
             _activeRoom.Node.GlobalPosition = position;
-            AddChild(_activeRoom.Node);
+
+            SetNodePaused(_activeRoom.Node, false);
+            _activeRoomHolder.AddChild(_activeRoom.Node);
 
             _camera.GlobalPosition = _activeRoom.Node.GlobalPosition;
         }

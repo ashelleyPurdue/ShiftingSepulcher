c25a1a659e59e77dd7c6c3e5acf048a368aa4764
Alex Shelley <alexthepeppershelley@gmail.com>
2022-10-21 16:36:42 -0400

    Add AllDescendants() and AllDescendantsOfType() extensions

--- Scripts/Utils/NodeExtensions.cs
+++ Scripts/Utils/NodeExtensions.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Godot;
 
 namespace RandomDungeons.Utils
@@ -18,6 +21,31 @@ namespace RandomDungeons.Utils
                 return (T)parent;
 
             return parent?.FindAncestor<T>();
+        }
+
+        public static IEnumerable<Node> AllDescendants(this Node n)
+        {
+            foreach (var child in n.GetChildren())
+            {
+                var childNode = (Node)child;
+                yield return childNode;
+
+                foreach (var descendant in childNode.AllDescendants())
+                    yield return descendant;
+            }
+        }
+
+        /// <summary>
+        /// Yields all descendants of this node that are of the specified type.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static IEnumerable<T> AllDescendantsOfType<T>(this Node n)
+        {
+            return n.AllDescendants()
+                .Where(d => d is T)
+                .Cast<T>();
         }
 
         /// <summary>

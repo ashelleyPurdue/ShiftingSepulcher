258c2137bb2d69697f2a192332b410b0c23d5300
Alex Shelley <alexthepeppershelley@gmail.com>
2022-10-25 19:08:41 -0400

    RoomFactory now uses SceneSpawnTable

--- Scripts/DungeonRooms/DungeonRoomFactory.cs
+++ Scripts/DungeonRooms/DungeonRoomFactory.cs
@@ -7,57 +7,35 @@ namespace RandomDungeons
 {
     public class DungeonRoomFactory : Node
     {
-        private const string RoomTemplatesFolder = "res://Scenes/Prefabs/DungeonRoomTemplates";
-        private const string DictHintString = "The key is the name of a .tscn file, minus the file extension, relative to " + RoomTemplatesFolder;
-
         [Export] public PackedScene EmptyRoom;
         [Export] public PackedScene KeyRoom;
 
-        [Export(hintString: DictHintString)]
-        public Dictionary<string, int> FillerRoomWeights;
-
-        [Export(hintString: DictHintString)]
-        public Dictionary<string, int> CombatRoomWeights;
-
-        [Export(hintString: DictHintString)]
-        public Dictionary<string, int> PuzzleRoomWeights;
-
-        [Export(hintString: DictHintString)]
-        public Dictionary<string, int> BossRoomWeights;
+        [Export] public SceneSpawnTable FillerRoomTable;
+        [Export] public SceneSpawnTable CombatRoomTable;
+        [Export] public SceneSpawnTable PuzzleRoomTable;
+        [Export] public SceneSpawnTable BossRoomTable;
 
         public IDungeonRoom BuildRoom(DungeonGraphRoom graphRoom)
         {
             switch (graphRoom.ChallengeType)
             {
-                case ChallengeType.None: return PickTemplateFrom(FillerRoomWeights, graphRoom);
+                case ChallengeType.None: return SpawnFromTable(FillerRoomTable, graphRoom);
                 case ChallengeType.Loot: return UseTemplate(KeyRoom, graphRoom);
-                case ChallengeType.Combat: return PickTemplateFrom(CombatRoomWeights, graphRoom);
-                case ChallengeType.Puzzle: return PickTemplateFrom(PuzzleRoomWeights, graphRoom);
-                case ChallengeType.Boss: return PickTemplateFrom(BossRoomWeights, graphRoom);
+                case ChallengeType.Combat: return SpawnFromTable(CombatRoomTable, graphRoom);
+                case ChallengeType.Puzzle: return SpawnFromTable(PuzzleRoomTable, graphRoom);
+                case ChallengeType.Boss: return SpawnFromTable(BossRoomTable, graphRoom);
 
                 default: return UseTemplate(EmptyRoom, graphRoom);
             }
         }
 
-        private IDungeonRoom BuildPuzzleRoom(DungeonGraphRoom graphRoom)
+        private IDungeonRoom SpawnFromTable(SceneSpawnTable table, DungeonGraphRoom graphRoom)
         {
-            return PickTemplateFrom(PuzzleRoomWeights, graphRoom);
-        }
-
-        private IDungeonRoom PickTemplateFrom(
-            Dictionary<string, int> templateWeights,
-            DungeonGraphRoom graphRoom
-        )
-        {
-            var weights = templateWeights
-                .Select(kvp => (value: kvp.Key, weight: kvp.Value))
-                .ToArray();
-
             var rng = new Random(graphRoom.RoomSeed);
-            string templatePath = $"{RoomTemplatesFolder}/{rng.PickFromWeighted(weights)}.tscn";
-            var template = GD.Load<PackedScene>(templatePath);
+            var realRoom = table.Spawn<IDungeonRoom>(rng);
+            realRoom.Populate(graphRoom);
 
-            return UseTemplate(template, graphRoom);
+            return realRoom;
         }
 
         private IDungeonRoom UseTemplate(PackedScene template, DungeonGraphRoom graphRoom)

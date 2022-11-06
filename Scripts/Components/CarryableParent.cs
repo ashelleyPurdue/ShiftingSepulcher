9a885a0f319fc40038e91eee3d97426915304a23
Alex Shelley <alexthepeppershelley@gmail.com>
2022-11-06 15:00:39 -0500

    Carryables can now stop themselves from rotating while carried, if enabled.

--- Scripts/Components/CarryableParent.cs
+++ Scripts/Components/CarryableParent.cs
@@ -10,8 +10,17 @@ namespace RandomDungeons
 
         public Node2D Node => GetParent<Node2D>();
 
+        [Export] public bool RotatesWhileCarried = true;
+
         private Node _originalParent;
+        private float _originalGlobalRotation;
         private bool _isBeingCarried = false;
+
+        public override void _Process(float delta)
+        {
+            if (_isBeingCarried && !RotatesWhileCarried)
+                Node.GlobalRotation = _originalGlobalRotation;
+        }
 
         public void PickUp(Node2D carrier)
         {
@@ -20,11 +29,14 @@ namespace RandomDungeons
 
             _isBeingCarried = true;
 
+            _originalGlobalRotation = Node.GlobalRotation;
             _originalParent = Node.GetParent();
+
             _originalParent.RemoveChild(Node);
             carrier.AddChild(Node);
 
             Node.Position = Vector2.Zero;
+
             EmitSignal(nameof(PickedUp));
         }
 
@@ -38,7 +50,9 @@ namespace RandomDungeons
             Node.GetParent().RemoveChild(Node);
             _originalParent.AddChild(Node);
 
+            Node.GlobalRotation = _originalGlobalRotation;
             Node.GlobalPosition = releasePosGlobal;
+
             EmitSignal(nameof(Released));
         }
     }

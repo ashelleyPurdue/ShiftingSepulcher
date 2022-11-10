eae3d80bb12428fd7e27a355120a602d362a30ff
Alex Shelley <alexthepeppershelley@gmail.com>
2022-11-09 20:05:01 -0500

    It now generates and creates the weights, but it's not being counted as solved?

--- Scripts/RoomPopulators/ScalePuzzle.cs
+++ Scripts/RoomPopulators/ScalePuzzle.cs
@@ -7,13 +7,60 @@ namespace RandomDungeons
 {
     public class ScalePuzzle : Node2D, IChallenge, IRoomPopulator
     {
+        [Export] public int MinWeightSize = 1;
+        [Export] public int MaxWeightSize = 3;
+        [Export] public int MinWeightCount = 3;
+        [Export] public int MaxWeightCount = 5;
+        [Export] public PackedScene CarryableWeightPrefab;
+
         private Area2D _leftZone => GetNode<Area2D>("%LeftScaleZone");
         private Area2D _rightZone => GetNode<Area2D>("%RightScaleZone");
 
-        private IEnumerable<CarryableWeights> _weights;
+        private List<CarryableWeights> _weights = new List<CarryableWeights>();
 
         public void Populate(DungeonGraphRoom graphRoom, Random rng)
         {
+            var leftSide = new List<int>();
+            var rightSide = new List<int>();
+
+            int weightCount = rng.Next(MinWeightCount, MaxWeightCount + 1);
+
+            // Keep adding randomly-sized weights to the lightest side
+            for (int i = 0; i < weightCount - 1; i++)
+            {
+                int weight = rng.Next(MinWeightSize, MaxWeightSize + 1);
+                var side = LightestSideOrCoinFlip();
+                side.Add(weight);
+            }
+
+            // Equalize them
+            int diff = Math.Abs(leftSide.Sum() - rightSide.Sum());
+            if (diff != 0)
+                LightestSideOrCoinFlip().Add(diff);
+
+            // Create the real weights
+            foreach (int weight in leftSide.Concat(rightSide))
+            {
+                var weightObj = CarryableWeightPrefab.Instance<CarryableWeights>();
+                weightObj.NumWeights = weight;
+
+                AddChild(weightObj);
+                _weights.Add(weightObj);
+            }
+
+            List<int> LightestSideOrCoinFlip()
+            {
+                int leftWeight = leftSide.Sum(w => w);
+                int rightWeight = rightSide.Sum(w => w);
+
+                if (leftWeight == rightWeight)
+                    return rng.PickFrom(new[] {leftSide, rightSide});
+
+                if (leftWeight < rightWeight)
+                    return leftSide;
+                else
+                    return rightSide;
+            }
         }
 
         public bool IsSolved()

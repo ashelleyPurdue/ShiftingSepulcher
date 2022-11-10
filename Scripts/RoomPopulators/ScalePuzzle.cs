e18b83863a2f5a41175baf5b72539b943c036891
Alex Shelley <alexthepeppershelley@gmail.com>
2022-11-09 19:43:30 -0500

    Add TotalWeightIn()

--- Scripts/RoomPopulators/ScalePuzzle.cs
+++ Scripts/RoomPopulators/ScalePuzzle.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Linq;
 using Godot;
 
 namespace RandomDungeons
 {
     public class ScalePuzzle : Node2D, IChallenge, IRoomPopulator
     {
+        private Area2D _leftZone => GetNode<Area2D>("%LeftScaleZone");
+        private Area2D _rightZone => GetNode<Area2D>("%RightScaleZone");
+
         public void Populate(DungeonGraphRoom graphRoom, Random rng)
         {
         }
@@ -12,6 +16,14 @@ namespace RandomDungeons
         public bool IsSolved()
         {
             return true;
+        }
+
+        private int TotalWeightIn(Area2D zone)
+        {
+            return zone
+                .GetOverlappingAreas()
+                .OfType<CarryableWeights>()
+                .Sum(w => w.NumWeights);
         }
     }
 }

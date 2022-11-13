40758e7bd2233da895061428e9fda20f742d6371
Alex Shelley <ashelley@purdue.edu>
2022-11-13 16:40:19 -0500

    Add throwing logic to BreakablePot, but there's nothing to trigger it yet.

--- Scripts/Elements/BreakablePot.cs
+++ Scripts/Elements/BreakablePot.cs
@@ -2,18 +2,49 @@ using Godot;
 
 namespace RandomDungeons
 {
-    public class BreakablePot : Node2D
+    public class BreakablePot : KinematicBody2D
     {
+        [Export] public float ThrowDistance = 32 * 5;
+        [Export] public float ThrowFlyTime = 1;
+
         private AnimationPlayer _animator => GetNode<AnimationPlayer>("%AnimationPlayer");
         private bool _isDead = false;
+        private bool _isFlying = false;
+        private Vector2 _velocity;
+
+        public override void _PhysicsProcess(float delta)
+        {
+            if (!_isFlying)
+                return;
+
+            var collision = MoveAndCollide(_velocity * delta);
+            if (collision != null)
+            {
+                Shatter();
+            }
+        }
+
+        public void OnThrown(Vector2 direction)
+        {
+            _isFlying = true;
+            _velocity = direction * (ThrowDistance / ThrowFlyTime);
+
+            GetTree()
+                .CreateTimer(ThrowFlyTime)
+                .Connect("timeout", this, nameof(Shatter));
+        }
 
         public void OnTookDamage(HitBox hitBox)
         {
             if (!_isDead)
-            {
-                _isDead = true;
-                _animator.Play("Shatter");
-            }
+                Shatter();
+        }
+
+        private void Shatter()
+        {
+            _isDead = true;
+            _isFlying = false;
+            _animator.Play("Shatter");
         }
     }
 }

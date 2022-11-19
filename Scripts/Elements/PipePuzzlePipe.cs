25c336b2290f590f09c6ce332656fe77a7c9b47b
Alex Shelley <alexthepeppershelley@gmail.com>
2022-11-19 17:05:07 -0500

    Pipes now have a knob that rotates them when hit with the sword

--- Scripts/Elements/PipePuzzlePipe.cs
+++ Scripts/Elements/PipePuzzlePipe.cs
@@ -6,12 +6,22 @@ namespace RandomDungeons
     {
         public PipePuzzleGraph.Cell Cell;
 
+        private AnimationPlayer _animator => GetNode<AnimationPlayer>("%Animator");
+
         public override void _Process(float delta)
         {
             foreach (var dir in CardinalDirectionUtils.All())
             {
                 GetPipeDisplay(dir).Visible = Cell.IsDirectionOpen(dir);
             }
+        }
+
+        public void OnKnobHit(HitBox hitbox)
+        {
+            Cell.RotateClockwise();
+            _animator.Play("RESET");
+            _animator.Advance(0);
+            _animator.Play("RotateClockwise");
         }
 
         private Node2D GetPipeDisplay(CardinalDirection dir)

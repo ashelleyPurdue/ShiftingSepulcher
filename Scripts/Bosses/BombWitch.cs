eda2000650fb3028e40d7ee2b45bbb4bc69e7784
Alex Shelley <ashelley@purdue.edu>
2022-11-27 18:43:03 -0500

    She now rotates towards the player while aiming

--- Scripts/Bosses/BombWitch.cs
+++ Scripts/Bosses/BombWitch.cs
@@ -6,8 +6,31 @@ namespace RandomDungeons
     public class BombWitch : Node
     {
         [Export] public float TeleportRadius = 6 * 32;
+        [Export] public bool RotateTowardPlayer = false;
 
         private AnimationPlayer _animator => GetNode<AnimationPlayer>("%AnimationPlayer");
+        private Node2D _body => GetParent<Node2D>();
+
+        public override void _PhysicsProcess(float delta)
+        {
+            var player = GetTree().FindPlayer();
+
+            // Rotate toward the player while aiming
+            if (RotateTowardPlayer && player != null)
+            {
+                float targetRotRad = _body
+                    .GlobalPosition
+                    .AngleToPoint(player.GlobalPosition);
+
+                targetRotRad -= Mathf.Deg2Rad(90 + 180);
+
+                _body.Rotation = Mathf.LerpAngle(
+                    _body.Rotation,
+                    targetRotRad,
+                    0.125f
+                );
+            }
+        }
 
         public void ResetAnimator()
         {
@@ -19,8 +42,7 @@ namespace RandomDungeons
             float angle = GD.Randf() * Mathf.Deg2Rad(360);
             float radius = GD.Randf() * TeleportRadius;
 
-            var parent = GetParent<Node2D>();
-            parent.Position = new Vector2(
+            _body.Position = new Vector2(
                 radius * Mathf.Cos(angle),
                 radius * Mathf.Sin(angle)
             );

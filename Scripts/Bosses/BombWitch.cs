146d4a46604f23f4be4a98ccbe96b440b48c075a
Alex Shelley <ashelley@purdue.edu>
2022-11-27 18:25:29 -0500

    She now teleports to a random spot

--- Scripts/Bosses/BombWitch.cs
+++ Scripts/Bosses/BombWitch.cs
@@ -5,11 +5,25 @@ namespace RandomDungeons
 {
     public class BombWitch : Node
     {
+        [Export] public float TeleportRadius = 6 * 32;
+
         private AnimationPlayer _animator => GetNode<AnimationPlayer>("%AnimationPlayer");
 
         public void ResetAnimator()
         {
             _animator.Reset();
+        }
+
+        public void TeleportToRandomSpot()
+        {
+            float angle = GD.Randf() * Mathf.Deg2Rad(360);
+            float radius = GD.Randf() * TeleportRadius;
+
+            var parent = GetParent<Node2D>();
+            parent.Position = new Vector2(
+                radius * Mathf.Cos(angle),
+                radius * Mathf.Sin(angle)
+            );
         }
     }
 }

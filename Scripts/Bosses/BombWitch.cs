31188bd7d7d70176a5516f71a7717daae6c0fb31
Alex Shelley <ashelley@purdue.edu>
2022-11-27 18:05:14 -0500

    Add a basic loop of moves

--- Scripts/Bosses/BombWitch.cs
+++ Scripts/Bosses/BombWitch.cs
@@ -6,5 +6,20 @@ namespace RandomDungeons
     public class BombWitch : Node
     {
         private AnimationPlayer _animator => GetNode<AnimationPlayer>("%AnimationPlayer");
+
+        public void TeleportAway()
+        {
+            _animator.ResetAndPlay("TeleportAway");
+        }
+
+        public void TeleportIn()
+        {
+            _animator.ResetAndPlay("TeleportIn");
+        }
+
+        public void Throw()
+        {
+            _animator.ResetAndPlay("Throw");
+        }
     }
 }

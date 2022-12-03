6fc218b44e05e44de841dbb0b27eb00855c35af5
Alex Shelley <alexthepeppershelley@gmail.com>
2022-12-03 16:44:51 -0500

    Add bomb script

---
+++ Scripts/Projectiles/Bomb.cs
@@ -0,0 +1,40 @@
+using Godot;
+
+namespace RandomDungeons
+{
+    public class Bomb : Node2D
+    {
+        [Export] public PackedScene ExplosionPrefab;
+        [Export] public float FuseDuration = 3;
+
+        private bool _isFuseLit = false;
+        private float _fuseTimer;
+
+        public override void _PhysicsProcess(float delta)
+        {
+            if (!_isFuseLit)
+                return;
+
+            _fuseTimer -= delta;
+
+            if (_fuseTimer <= 0)
+                Detonate();
+        }
+
+        public void LightFuse()
+        {
+            _fuseTimer = FuseDuration;
+            _isFuseLit = true;
+        }
+
+        public void Detonate()
+        {
+            var parent = GetParent<Node2D>();
+            var explosion = ExplosionPrefab.Instance<Node2D>();
+            parent.AddChild(explosion);
+            explosion.Position = Position;
+
+            QueueFree();
+        }
+    }
+}

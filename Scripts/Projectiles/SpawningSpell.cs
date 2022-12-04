8d669019b2a091c31f6a3f1004b8e62a5f0aa917
Alex Shelley <alexthepeppershelley@gmail.com>
2022-12-04 12:24:07 -0500

    Add SpawningSpell script

---
+++ Scripts/Projectiles/SpawningSpell.cs
@@ -0,0 +1,24 @@
+using Godot;
+
+namespace RandomDungeons
+{
+    public class SpawningSpell : Node2D
+    {
+        [Export] public float Speed = 32 * 4;
+        [Export] public Vector2 TargetPosGlobal;
+
+        public Node2D NodeToSpawn;
+
+        public override void _PhysicsProcess(float delta)
+        {
+            GlobalPosition = GlobalPosition.MoveToward(TargetPosGlobal, Speed * delta);
+
+            if (GlobalPosition == TargetPosGlobal)
+            {
+                GetParent().AddChild(NodeToSpawn);
+                NodeToSpawn.GlobalPosition = TargetPosGlobal;
+                QueueFree();
+            }
+        }
+    }
+}

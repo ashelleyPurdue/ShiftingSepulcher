986b2f0bb5d546f993b5ac7c4375b21af1db0845
Alex Shelley <alexthepeppershelley@gmail.com>
2022-12-20 22:13:00 -0500

    Recovery hearts now despawn when the player dies

--- Scripts/ItemPickups/RecoveryHeart.cs
+++ Scripts/ItemPickups/RecoveryHeart.cs
@@ -2,7 +2,7 @@ using Godot;
 
 namespace RandomDungeons
 {
-    public class RecoveryHeart : Node2D
+    public class RecoveryHeart : Node2D, IRespawnable
     {
         public void OnPickedUp()
         {
@@ -11,6 +11,14 @@ namespace RandomDungeons
             if (PlayerInventory.Health > PlayerInventory.MaxHealth)
                 PlayerInventory.Health = PlayerInventory.MaxHealth;
 
+            QueueFree();
+        }
+
+        public void Respawn()
+        {
+            // Despawn the heart whenever the player dies.
+            // This way, the player can't keep suiciding and re-killing enemies
+            // to create a ton of hearts on the ground.
             QueueFree();
         }
     }

2250372c6c30bb6a57e72495928af048e5b0f421
Alex Shelley <alexthepeppershelley@gmail.com>
2022-12-26 18:45:01 -0500

    Add RoomEntrance and GetEntrance()

--- Scripts/Room2D.cs
+++ Scripts/Room2D.cs
@@ -1,9 +1,15 @@
+using System;
+using System.Linq;
 using Godot;
 
 namespace RandomDungeons
 {
     public class Room2D : Node2D
     {
-
+        public RoomEntrance GetEntrance(string name)
+        {
+            return this.AllDescendantsOfType<RoomEntrance>()
+                .First(e => e.Name == name);
+        }
     }
 }

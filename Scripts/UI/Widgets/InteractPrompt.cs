3df21a3e7a7df1b90795561315977c0bbc65f257
Alex Shelley <alexthepeppershelley@gmail.com>
2023-01-28 19:30:29 -0500

    Add a little animation to the prompt

--- Scripts/UI/Widgets/InteractPrompt.cs
+++ Scripts/UI/Widgets/InteractPrompt.cs
@@ -4,22 +4,40 @@ namespace RandomDungeons
 {
     public class InteractPrompt : Node2D
     {
+        private AnimationPlayer _animator => GetNode<AnimationPlayer>("%AnimationPlayer");
         private Label _promptTextLabel => GetNode<Label>("%PromptTextLabel");
+
+        private bool _wasVisible = false;
+        private Vector2 _pos;
+        private Vector2 _targetPos;
 
         public override void _Process(float delta)
         {
             var player = GetTree().FindPlayer();
             var highlightedObject = player.HighlightedObject;
 
-            if (highlightedObject == null || player.IsHoldingSomething)
+            Visible = (highlightedObject != null) && !player.IsHoldingSomething;
+
+            if (Visible)
             {
-                Visible = false;
-                return;
+                _promptTextLabel.Text = highlightedObject.PromptText;
+                _targetPos = highlightedObject.PromptGlobalPosition;
             }
 
-            Visible = true;
-            _promptTextLabel.Text = highlightedObject.PromptText;
-            GlobalPosition = highlightedObject.PromptGlobalPosition;
+            // Play the animation when first appearing
+            if (Visible && !_wasVisible)
+            {
+                _animator.ResetAndPlay("Appear");
+                _pos = _targetPos;
+            }
+            _wasVisible = Visible;
+
+            // Glide to the appropriate position if the player changed targets
+            // without the prompt disappearing
+            float deltaPos = 32 * 60 * 0.25f * delta;
+            _pos = _pos.MoveToward(_targetPos, deltaPos);
+
+            GlobalPosition = _pos;
         }
     }
 }

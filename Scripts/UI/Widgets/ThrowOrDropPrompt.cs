716647affdcd32cc0da0b36595726dcbc4c44c72
Alex Shelley <alexthepeppershelley@gmail.com>
2023-01-29 16:01:48 -0500

    Add a prompt for throwing/dropping held objects

---
+++ Scripts/UI/Widgets/ThrowOrDropPrompt.cs
@@ -0,0 +1,34 @@
+using Godot;
+
+namespace RandomDungeons
+{
+    public class ThrowOrDropPrompt : Node2D
+    {
+        private AnimationPlayer _animator => GetNode<AnimationPlayer>("%AnimationPlayer");
+        private bool _wasHolding = false;
+
+        public override void _Ready()
+        {
+            // Hide the prompt until the first time the player picks something
+            // up.  It will then remain "visible" forever, although the animation
+            // will shrink it to size 0 when the player drops it.
+            Visible = false;
+        }
+
+        public override void _Process(float delta)
+        {
+            var player = GetTree().FindPlayer();
+
+            if (player.IsHoldingSomething && !_wasHolding)
+            {
+                _animator.ResetAndPlay("Appear");
+                Visible = true;
+            }
+
+            if (!player.IsHoldingSomething && _wasHolding)
+                _animator.PlayBackwards("Appear");
+
+            _wasHolding = player.IsHoldingSomething;
+        }
+    }
+}

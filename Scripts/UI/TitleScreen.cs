8a96da172b44148d2a518c89bc225f74ef3f0bc5
Alex Shelley <alexthepeppershelley@gmail.com>
2023-01-31 18:26:56 -0500

    Allow the intro to be skipped

--- Scripts/UI/TitleScreen.cs
+++ Scripts/UI/TitleScreen.cs
@@ -10,13 +10,28 @@ namespace RandomDungeons
 
         [Export] public PackedScene DungeonModeScene;
 
+        [Export] public bool AllowSkipIntro = true;
+
+        private Control _options => GetNode<Control>("%Options");
         private Control _defaultFocusedOption => GetNode<Control>(DefaultFocusedOption);
+
+        private AnimationPlayer _animationPlayer => GetNode<AnimationPlayer>("%AnimationPlayer");
         private SeedInput _seedInput => GetNode<SeedInput>("%SeedInput");
 
         public override void _Ready()
         {
-            _defaultFocusedOption.GrabFocus();
             MusicService.Instance.StopSong();
+        }
+
+        public override void _Input(InputEvent ev)
+        {
+            if (ev.IsPressed() && AllowSkipIntro)
+                SkipIntro();
+        }
+
+        public void SkipIntro()
+        {
+            _animationPlayer.ResetAndPlay("SkippedIntro");
         }
 
         private void PlayDungeonMode()

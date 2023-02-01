440dd008b1cd71fc200d76b63ecdd1dcece242c1
Alex Shelley <alexthepeppershelley@gmail.com>
2023-01-31 19:53:34 -0500

    The victory screen is now controller-accessible

--- Scripts/UI/VictoryScreen.cs
+++ Scripts/UI/VictoryScreen.cs
@@ -4,9 +4,12 @@ namespace RandomDungeons
 {
     public class VictoryScreen : Node
     {
+        private Button _backToTitleButton => GetNode<Button>("%BackToTitleButton");
+
         public override void _Ready()
         {
             MusicService.Instance.StopSong();
+            _backToTitleButton.GrabFocus();
         }
 
         private void BackToTitleButtonPressed()

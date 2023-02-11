aa9ac1692603b17f25484a7322a44f0de9af916a
Alex Shelley <alexthepeppershelley@gmail.com>
2023-02-11 16:59:25 -0500

    Group all the dungeon generator's parameters into a single object

---
+++ Scripts/DungeonGeneration/DungeonGeneratorParams.cs
@@ -0,0 +1,13 @@
+namespace ShiftingSepulcher
+{
+    public class DungeonGeneratorParams
+    {
+        public int NumRuns = 6;
+        public int MinRunLength = 3;
+        public int MaxRunLength = 5;
+
+        public int FillerRoomWeight = 1;
+        public int CombatRoomWeight = 3;
+        public int PuzzleRoomWeight = 2;
+    }
+}

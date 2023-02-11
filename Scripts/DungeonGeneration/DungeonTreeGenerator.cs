ffe0856b8953449bd811a93b4a30d10eb7af8325
Alex Shelley <alexthepeppershelley@gmail.com>
2023-02-11 17:17:25 -0500

    Make a separate method for generating the critical path

--- Scripts/DungeonGeneration/DungeonTreeGenerator.cs
+++ Scripts/DungeonGeneration/DungeonTreeGenerator.cs
@@ -18,43 +18,48 @@ namespace ShiftingSepulcher
             DungeonTreeRoom root = new DungeonTreeRoom();
             root.ChallengeType = ChallengeType.None;
 
-            DungeonTreeRoom prevRunRoot = root;
-            for (int runNumber = 0; runNumber < genParams.NumRuns; runNumber++)
-            {
-                int runLength = rng.Next(genParams.MinRunLength, genParams.MaxRunLength);
-                var runRoot = GenerateRun(runNumber, runLength);
-
-                // Pick a random room to start this run in.
-                // Don't pick the room where we hid this run's key, because that
-                // would result in a key being in the same room as the door it
-                // unlocks, which would look silly.
-                // TODO: Bias it toward rooms that are close to the previous
-                // run's root, to minimize backtracking
-                var availableRooms = root
-                    .AllDescendants()
-                    .Where(r => r.ChildDoors.Count < 3)
-                    .Where(r => runNumber == 0 || r.KeyId != runNumber);
-                var runStartRoom = rng.PickFrom(availableRooms);
-
-                // Lock the door to this run, unless it's the first run of the
-                // dungeon
-                if (runNumber != 0)
-                    runStartRoom.AddLockedDoor(runRoot, runNumber);
-                else
-                    runStartRoom.AddChallengeDoor(runRoot);
-
-                prevRunRoot = runRoot;
-            }
-
-            // Mark the final room of the final run as the boss room
-            var bossRoom = prevRunRoot
-                .AllDescendants()
-                .Last();
-
-            bossRoom.ChallengeType = ChallengeType.Boss;
-            bossRoom.KeyId = 0;
+            GenerateCriticalPath();
 
             return root;
+
+            void GenerateCriticalPath()
+            {
+                DungeonTreeRoom prevRunRoot = root;
+                for (int runNumber = 0; runNumber < genParams.NumRuns; runNumber++)
+                {
+                    int runLength = rng.Next(genParams.MinRunLength, genParams.MaxRunLength);
+                    var runRoot = GenerateRun(runNumber, runLength);
+
+                    // Pick a random room to start this run in.
+                    // Don't pick the room where we hid this run's key, because that
+                    // would result in a key being in the same room as the door it
+                    // unlocks, which would look silly.
+                    // TODO: Bias it toward rooms that are close to the previous
+                    // run's root, to minimize backtracking
+                    var availableRooms = root
+                        .AllDescendants()
+                        .Where(r => r.ChildDoors.Count < 3)
+                        .Where(r => runNumber == 0 || r.KeyId != runNumber);
+                    var runStartRoom = rng.PickFrom(availableRooms);
+
+                    // Lock the door to this run, unless it's the first run of the
+                    // dungeon
+                    if (runNumber != 0)
+                        runStartRoom.AddLockedDoor(runRoot, runNumber);
+                    else
+                        runStartRoom.AddChallengeDoor(runRoot);
+
+                    prevRunRoot = runRoot;
+                }
+
+                // Mark the final room of the final run as the boss room
+                var bossRoom = prevRunRoot
+                    .AllDescendants()
+                    .Last();
+
+                bossRoom.ChallengeType = ChallengeType.Boss;
+                bossRoom.KeyId = 0;
+            }
 
             DungeonTreeRoom GenerateRun(int runNumber, int runLength)
             {

a48e5569a4699d938f939797d78c6bf33273066d
Alex Shelley <ashelley@purdue.edu>
2023-02-12 18:58:42 -0500

    The minimap can now display multiple floors.  It also shows stairs!

--- Scripts/UI/Widgets/Minimap/DoorDisplay.cs
+++ Scripts/UI/Widgets/Minimap/DoorDisplay.cs
@@ -5,10 +5,14 @@ namespace ShiftingSepulcher
     [Tool]
     public class DoorDisplay : Node2D
     {
-        public void SetDoor(IDungeonTreeDoor door)
+        public void SetDoor(DungeonLayoutRoom room, IDungeonTreeDoor door)
         {
-            bool isWall = (door?.Destination == null);
-            Visible = !isWall;
+            if (door == null)
+            {
+                Visible = false;
+                return;
+            }
+            Visible = true;
 
             GetNode<Node2D>("%OneWayIcon").Visible = door is IncomingShortcutDoor;
             GetNode<Node2D>("%Lock").Visible = door is LockedDoor;
@@ -17,6 +21,10 @@ namespace ShiftingSepulcher
             {
                 GetNode<Node2D>("%Lock").Modulate = KeyColors.ForId(k.KeyId);
             }
+
+            Vector3i destPos = room.Layout.CoordsOf(door.Destination);
+            GetNode<Node2D>("%StairsUpIcon").Visible = destPos.z > room.Position.z;
+            GetNode<Node2D>("%StairsDownIcon").Visible = destPos.z < room.Position.z;
         }
     }
 }
--- Scripts/UI/Widgets/Minimap/Minimap.cs
+++ Scripts/UI/Widgets/Minimap/Minimap.cs
@@ -10,9 +10,32 @@ namespace ShiftingSepulcher
         private const float RoomSize = 5 * 32;
 
         [Export] public PackedScene RoomDisplayPrefab;
+        [Export] public int VisibleFloor
+        {
+            get => _visibleFloor;
+            set
+            {
+                bool changed = _visibleFloor != value;
+                _visibleFloor = value;
+
+                if (changed && _layout != null)
+                    Refresh();
+            }
+        }
+
         private Node2D _rooms => GetNode<Node2D>("%Rooms");
 
+        private DungeonLayout _layout;
+        private int _visibleFloor;
+
         public void SetLayout(DungeonLayout layout)
+        {
+            _layout = layout;
+            VisibleFloor = 0;
+            Refresh();
+        }
+
+        private void Refresh()
         {
             // Clear out old rooms
             foreach (var child in _rooms.EnumerateChildren())
@@ -21,7 +44,11 @@ namespace ShiftingSepulcher
             }
 
             // Create new rooms
-            foreach (var layoutRoom in layout.AllRooms())
+            var roomsOnFloor = _layout
+                .AllRooms()
+                .Where(r => r.Position.z == _visibleFloor);
+
+            foreach (var layoutRoom in roomsOnFloor)
             {
                 var roomDisplay = RoomDisplayPrefab.Instance<RoomDisplay>();
 
@@ -36,8 +63,8 @@ namespace ShiftingSepulcher
             }
 
             // Keep the rooms centered
-            var xValues = layout.AllRooms().Select(r => r.Position.x * RoomSize);
-            var yValues = layout.AllRooms().Select(r => r.Position.y * RoomSize);
+            var xValues = _layout.AllRooms().Select(r => r.Position.x * RoomSize);
+            var yValues = _layout.AllRooms().Select(r => r.Position.y * RoomSize);
 
             Vector2 center = new Vector2(
                 (xValues.Min() + xValues.Max()) / 2,

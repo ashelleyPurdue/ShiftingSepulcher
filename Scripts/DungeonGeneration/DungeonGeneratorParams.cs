11bbcb2c81bc9fdef25b4c1bd730af3e60c7a5a4
Alex Shelley <alexthepeppershelley@gmail.com>
2023-02-26 13:36:12 -0500

    Shorten runs by 1 room, to compensate for the removal of key-only rooms.

--- Scripts/DungeonGeneration/DungeonGeneratorParams.cs
+++ Scripts/DungeonGeneration/DungeonGeneratorParams.cs
@@ -3,8 +3,8 @@ namespace ShiftingSepulcher
     public class DungeonGeneratorParams
     {
         public int NumRuns = 6;
-        public int MinRunLength = 3;
-        public int MaxRunLength = 5;
+        public int MinRunLength = 2;
+        public int MaxRunLength = 4;
 
         public int FillerRoomWeight = 1;
         public int CombatRoomWeight = 3;

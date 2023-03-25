7028afb44de2bbba5613ad56264fe0bf07469ef5
Alex Shelley <alexthepeppershelley@gmail.com>
2023-03-25 17:49:27 -0400

    The gold counter now increases 1 at a time

--- Scripts/UI/Widgets/PlayerGoldDisplay.cs
+++ Scripts/UI/Widgets/PlayerGoldDisplay.cs
@@ -4,9 +4,22 @@ namespace ShiftingSepulcher
 {
     public class PlayerGoldDisplay : Label
     {
+        private int _displayedGold = 0;
+
+        public override void _Ready()
+        {
+            _displayedGold = PlayerInventory.Gold;
+        }
+
         public override void _Process(float delta)
         {
             Text = $"Gold: {PlayerInventory.Gold}";
+        }
+
+        public override void _PhysicsProcess(float delta)
+        {
+            if (_displayedGold < PlayerInventory.Gold)
+                _displayedGold++;
         }
     }
 }

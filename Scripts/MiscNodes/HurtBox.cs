ad512139e0b2b927c8ea85fcd36b66f42887502e
Alex Shelley <ashelley@purdue.edu>
2023-03-26 18:01:27 -0400

    Fix an ironic null reference exception

--- Scripts/MiscNodes/HurtBox.cs
+++ Scripts/MiscNodes/HurtBox.cs
@@ -11,10 +11,26 @@ namespace ShiftingSepulcher
         [Export] public float RecoilDistance = 32;
         [Export] public bool Enabled = true;
 
-        public HealthPointsComponent HealthPoints =>
-            string.IsNullOrEmpty(HealthPointsPath)
-                ? null
-                : GetNode<HealthPointsComponent>(HealthPointsPath);
+        public HealthPointsComponent HealthPoints
+        {
+            get
+            {
+                // Fun fact: string.IsNullOrEmpty() throws a null reference
+                // exception if you pass in a null NodePath.
+                //
+                // "You had ONE job, string.IsNullOrEmpty()!!!"
+                //
+                // For real though: the null ref exception happens because
+                // Godot tries to implicitly cast NodePath to a string, but the
+                // implicit cast operator wasn't written to expect nulls.
+                if (HealthPointsPath == null || HealthPointsPath.IsEmpty())
+                {
+                    return null;
+                }
+
+                return GetNode<HealthPointsComponent>(HealthPointsPath);
+            }
+        }
 
         private HashSet<HitBox> _ignoredHitboxes = new HashSet<HitBox>();
 

b22009a776f3d8ef9132aebba31e1ce11717679a
Alex Shelley <alexthepeppershelley@gmail.com>
2023-04-08 16:13:50 -0400

    The summoner now always picks a smart place to summon the minion, based on the player's position

--- Scripts/Bosses/StatueSummoner.cs
+++ Scripts/Bosses/StatueSummoner.cs
@@ -4,12 +4,15 @@ namespace ShiftingSepulcher
 {
     public class StatueSummoner : KinematicBody2D
     {
+        [Export] public float SummonAngleDeg = 90;
+        [Export] public float MaxSummonRadius = 32 * 100;
+
         [Export] public float MinionSummonInterval = 5;
 
         private MinionManager _minionManager => GetNode<MinionManager>("%MinionManager");
+        private RayCast2D _minionSpawnRay => GetNode<RayCast2D>("%MinionSpawnRay");
 
         private float _summonTimer = 0;
-
 
         public override void _PhysicsProcess(float delta)
         {
@@ -31,16 +34,38 @@ namespace ShiftingSepulcher
         private void SummonMinion()
         {
             var minion = _minionManager.SummonMinion();
+            minion.GlobalPosition = GetSummonPosition();
+        }
 
-            float spawnRadius = 100;
-            float angleRad = Mathf.Deg2Rad((float)GD.RandRange(0, 360));
+        private Vector2 GetSummonPosition()
+        {
+            Player aggroTarget = this.GetTree().FindPlayer();
 
-            minion.Position = new Vector2(
-                spawnRadius * Mathf.Cos(angleRad),
-                spawnRadius * Mathf.Sin(angleRad)
+            // Choose a random angle for the minion to spawn at, relative to the
+            // target.
+            float angleOffsetDeg = (float)GD.RandRange(
+                -SummonAngleDeg / 2,
+                SummonAngleDeg / 2
+            );
+            float angleOffsetRad = Mathf.Deg2Rad(angleOffsetDeg);
+
+            float angleToTargetRad = aggroTarget.GlobalPosition.AngleToPoint(GlobalPosition);
+            float angleRad = angleOffsetRad + angleToTargetRad;
+
+            Vector2 summonDir = new Vector2(
+                Mathf.Cos(angleRad),
+                Mathf.Sin(angleRad)
             );
 
-            minion.Position += this.GetPosRelativeToAncestor(this.GetRoom());
+            // Do a raycast to ensure the minion is spawned inside the room
+            _minionSpawnRay.ClearExceptions();
+            _minionSpawnRay.AddException(aggroTarget);
+            _minionSpawnRay.GlobalPosition = aggroTarget.GlobalPosition;
+            _minionSpawnRay.CastTo = summonDir * MaxSummonRadius;
+            _minionSpawnRay.ForceUpdateTransform();
+            _minionSpawnRay.ForceRaycastUpdate();
+
+            return _minionSpawnRay.GetCollisionPoint();
         }
     }
 }

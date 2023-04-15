3a1053762f38fa6e1d586226fc85364eac977eb6
Alex Shelley <alexthepeppershelley@gmail.com>
2023-04-15 11:56:28 -0400

    Add PauseState

---
+++ Scripts/StateMachines/PauseState.cs
@@ -0,0 +1,26 @@
+using System;
+using Godot;
+
+namespace ShiftingSepulcher
+{
+    public abstract class PauseState<TOwner> : State<TOwner>
+    {
+        public abstract float Duration {get;}
+        public abstract IState NextState {get;}
+
+        private float _timer;
+
+        public override void _StateEntered()
+        {
+            _timer = Duration;
+        }
+
+        public override void _PhysicsProcess(float delta)
+        {
+            _timer -= delta;
+
+            if (_timer <= 0)
+                ChangeState(NextState);
+        }
+    }
+}
